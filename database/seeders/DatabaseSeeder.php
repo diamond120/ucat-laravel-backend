b7d626890905e6a20f0af274420b132400a7e127
diamond312 <diamond.at312@gmail.com>
2024-05-24 07:38:58 -0700

    add code field in seed data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -43,7 +43,7 @@ class DatabaseSeeder extends Seeder
             $package_id = DB::table('packages')->insertGetId([
                 'name' => $package->name,
                 'type' => $package->type,
-                'code' => '1234',
+                'code' => $package->code,
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
